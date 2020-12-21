5642142550c314c6fc21393d6adc0cc752d34310
TsAnastasia <72159790+TsAnastasia@users.noreply.github.com>
2020-12-21 21:01:34 +0300

    feat: restyle categories

--- scripts/addInitiative.js
+++ scripts/addInitiative.js
@@ -1,11 +1,18 @@
 //TO DO: data from server
 const categories = [
-  {linkImage: "", title: "Дом"},
-  {linkImage: "", title: "Улица"},
-  {linkImage: "", title: "Фонарь"},
+  {name: "Двор", icon: "./images/category/yard.svg"},
+  {name: "Дом", icon: "./images/category/home.svg"},
+  {name: "Дорога", icon: "./images/category/road.svg"},
+  {name: "Инфраструктура", icon: "./images/category/infrastructure.svg"},
+  {name: "Транспорт", icon: "./images/category/transport.svg"},
+  {name: "Строительство", icon: "./images/category/bulding.svg"},
+  {name: "Торговля и реклама", icon: "./images/category/marketing.svg"},
+  {name: "Госучереждения и общественные организации", icon: "./images/category/institution.svg"},
+  {name: "Поликлиники", icon: "./images/category/hospital.svg"},
+  {name: "Эко", icon: "./images/category/eco.svg"}
 ];
 
-const templaleInitiativeCard = page.querySelector('.templeteCategoryCard').content;
+const templeteCategoryCard = page.querySelector('.templeteCategoryCard').content;
 const sections = [...page.querySelectorAll('.section')];
 const categoriesSection = page.querySelector('.categories');
 const addInitiativeSection = page.querySelector('.addInitiative');
@@ -20,13 +27,13 @@ const buttonBack = page.querySelector(".previe__buttonBack");
 
 //category: title, description
 const createCategoryCard = (category) => {
-  const newCard = templaleInitiativeCard.cloneNode(true);
+  const newCard = templeteCategoryCard.cloneNode(true);
   const imageContainer = newCard.querySelector('.categoryCard__image');
   const titleContainer = newCard.querySelector('.categoryCard__title');
-  const container = newCard.querySelector('.categoryCard');
-  imageContainer.src = category.linkImage;
-  titleContainer.textContent = category.title;
-  container.addEventListener('click', () => {
+  imageContainer.src = category.icon;
+  imageContainer.alt = category.name;
+  titleContainer.textContent = category.name;
+  newCard.addEventListener('click', () => {
     chooseCategory(category);
   });
   return newCard;
@@ -70,17 +77,17 @@ navLinkCategoryContainer.addEventListener('click', () => {
   backtoCategory();
 });
 
-formContainer.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  submitFormAddInitiative();
-});
+// formContainer.addEventListener('submit', (evt) => {
+//   evt.preventDefault();
+//   submitFormAddInitiative();
+// });
 
-buttonPrevie.addEventListener('click', () =>{
-  showPrevie();
-});
+// buttonPrevie.addEventListener('click', () =>{
+//   showPrevie();
+// });
 
-buttonBack.addEventListener('click', () =>{
-  changeSection(addInitiativeSection);
-});
+// buttonBack.addEventListener('click', () =>{
+//   changeSection(addInitiativeSection);
+// });
 
 addCategoryCards(categories, categoriesContainer);
