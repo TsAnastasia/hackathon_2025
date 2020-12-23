5ff783c7daa885c3c0f5f68a2d79a054ffd595d4
TsAnastasia <72159790+TsAnastasia@users.noreply.github.com>
2020-12-23 11:56:50 +0300

    feat: do validate form and preview

--- scripts/addInitiative.js
+++ scripts/addInitiative.js
@@ -14,41 +14,45 @@ const categories = [
 
 const templeteCategoryCard = page.querySelector('.templeteCategoryCard').content;
 const sections = [...page.querySelectorAll('.section')];
+const sectionHiddenClass = 'section_hidden';
 const categoriesSection = page.querySelector('.categories');
-const addInitiativeSection = page.querySelector('.addInitiative');
-const previeSection = page.querySelector('.previe');
+const inputDataSection = page.querySelector('.inputData');
 const successSection = page.querySelector('.success');
 const categoriesContainer = page.querySelector('.categories__list');
 const navLinkInitiativeContainer = page.querySelector('.nav__item_type_initiative');
 const navLinkCategoryContainer = page.querySelector('.nav__item_type_category');
-const formContainer = document.forms.addInitiative;
-const buttonPrevie = page.querySelector('.addInitiative__buttonPrevie');
-const buttonBack = page.querySelector(".previe__buttonBack");
+const formContainer = page.querySelector('.form');
+const buttonPreview = page.querySelector(".form__previewButton");
+const formElementCategory = formContainer.elements.category;
+const commentContainer = page.querySelector(".form__item_type_textarea");
+const previewContainer = page.querySelector('.form__preview');
 
-//category: title, description
 const createCategoryCard = (category) => {
   const newCard = templeteCategoryCard.cloneNode(true);
   const imageContainer = newCard.querySelector('.categoryCard__image');
   const titleContainer = newCard.querySelector('.categoryCard__title');
+  const cardContainer = newCard.querySelector('.categoryCard');
   imageContainer.src = category.icon;
   imageContainer.alt = category.name;
   titleContainer.textContent = category.name;
-  newCard.addEventListener('click', () => {
+  cardContainer.addEventListener('click', () => {
     chooseCategory(category);
   });
   return newCard;
 };
 
-const changeSection = (newSection) =>{
-  sections.forEach((section) => {
-    section.classList.add('section_hidden');
-  });
-  newSection.classList.remove('section_hidden');
+const chooseCategory = (category) => {
+  openForm(formContainer);
+  formElementCategory.value = category.name;
+  navLinkInitiativeContainer.classList.remove('nav__item_disable');
+  changeSection(inputDataSection);
 };
 
-const chooseCategory = (category) => {
-  changeSection(addInitiativeSection);
-  navLinkInitiativeContainer.classList.remove('nav__item_disable');
+const changeSection = (newSection) =>{
+  sections.forEach((section) => {
+    section.classList.add(sectionHiddenClass);
+  });
+  newSection.classList.remove(sectionHiddenClass);
 };
 
 const addCategoryCards = (categories, container) => {
@@ -58,8 +62,44 @@ const addCategoryCards = (categories, container) => {
 };
 
 const backtoCategory = () => {
-  changeSection(categoriesSection);
   navLinkInitiativeContainer.classList.add('nav__item_disable');
+  changeSection(categoriesSection);
+};
+
+const togglePreview = () => {
+  if (buttonPreview.classList.contains('form__previewButton_open')) {
+    previewContainer.textContent = '';
+  }else{
+    previewContainer.append(transformTextToQuote(commentContainer.value));
+  };
+  buttonPreview.classList.toggle('form__previewButton_open');
+  previewContainer.classList.toggle('form__preview_hidden');
+}
+
+//TO DO: использовать одну цитату, несколько раз, а не каждого символа - своя цитата
+const transformTextToQuote = (text) =>{
+  const qoute = 'Cъешь ещё этих мягких французских булок, да выпей чаю';
+  const resalt = document.createElement('div');
+  const templateQuteContainer = document.createElement('p');
+  templateQuteContainer.classList.add('quote');
+  const simvolContainer = document.createElement('span');
+  simvolContainer.classList.add('quote__bold');
+  text.toLowerCase().split('').forEach((simvol) =>{
+    const qouteContainer = templateQuteContainer.cloneNode(true);
+    if (simvol === ' '){
+      simvolContainer.textContent = '_';
+      qouteContainer.append(simvolContainer.cloneNode(true));
+    }else{
+      simvolContainer.textContent = simvol;
+      const index = qoute.indexOf(simvol);
+      qouteContainer.append(qoute.slice(0, index));
+      qouteContainer.append(simvolContainer.cloneNode(true));
+      qouteContainer.append(qoute.slice(index + 1, qoute.length));
+      qouteContainer.append('\n');
+    };
+    resalt.append(qouteContainer);
+  })
+  return resalt;
 };
 
 const submitFormAddInitiative = () => {
@@ -67,27 +107,15 @@ const submitFormAddInitiative = () => {
   changeSection(successSection);
 };
 
-const showPrevie = () => {
-  
-  changeSection(previeSection);
-}
+navLinkCategoryContainer.addEventListener('click', backtoCategory);
 
-
-navLinkCategoryContainer.addEventListener('click', () => {
-  backtoCategory();
+buttonPreview.addEventListener('click', () =>{
+  togglePreview();
 });
 
 // formContainer.addEventListener('submit', (evt) => {
 //   evt.preventDefault();
 //   submitFormAddInitiative();
-// });
-
-// buttonPrevie.addEventListener('click', () =>{
-//   showPrevie();
-// });
-
-// buttonBack.addEventListener('click', () =>{
-//   changeSection(addInitiativeSection);
 // });
 
 addCategoryCards(categories, categoriesContainer);
---
+++ scripts/validate.js
@@ -0,0 +1,82 @@
+const settingValidate = {
+  formSelector: '.form',
+  inputSelector: '.form__item',
+  infoSelector: '.form__infoRequired',
+  submitButtonSelector: '.form__submit',
+  inputErrorClass: 'form__item_error',
+  errorClass: '.form__error_type_',
+  infoClass: 'form__infoRequired_hidden',
+  previewButtonSelector: '.form__previewButton',
+  commentClass: 'form__item_type_textarea'
+};
+
+const openForm = (formElement) => {
+  const inputList = [...formElement.querySelectorAll(settingValidate.inputSelector)];
+  formElement.reset();
+  toggleButtonState(formElement, inputList);
+  inputList.forEach( (inputElement) => {
+    hideInputError(formElement, inputElement);
+  });
+};
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`${settingValidate.errorClass}${inputElement.name}`);
+  inputElement.classList.add(settingValidate.inputErrorClass);
+  errorElement.textContent = errorMessage;
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`${settingValidate.errorClass}${inputElement.name}`);
+  inputElement.classList.remove(settingValidate.inputErrorClass);
+  if (errorElement) { 
+    errorElement.textContent = '';
+  };
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  !inputElement.validity.valid ? showInputError(formElement, inputElement, inputElement.validationMessage) : hideInputError(formElement, inputElement);
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
+
+const toggleButtonState = (formElement, inputList) => {
+  const buttonElement = formElement.querySelector(settingValidate.submitButtonSelector);
+  const infoElement = formElement.querySelector(settingValidate.infoSelector);
+  if (hasInvalidInput(inputList)) {
+    buttonElement.setAttribute('disabled', true);
+    infoElement.classList.remove(settingValidate.infoClass);
+  }else{
+    buttonElement.removeAttribute('disabled');
+    infoElement.classList.add(settingValidate.infoClass);
+  };
+  toggleButtonPreview(formElement);
+};
+
+const toggleButtonPreview = (formElement) => {
+  const buttonPreview = formElement.querySelector(settingValidate.previewButtonSelector);
+  formElement.elements.comment.validity.valid ? buttonPreview.removeAttribute('disabled') : buttonPreview.setAttribute('disabled', true);
+};
+
+const enableValidation = () => {
+  formElement = document.querySelector(settingValidate.formSelector);
+  formElement.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+  });   
+  const inputList = [...formElement.querySelectorAll(settingValidate.inputSelector)];
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', () =>{
+      if (inputElement.classList.contains(settingValidate.commentClass)) {
+        const res = /[^А-Яа-яЁё ]/g.exec(inputElement.value);
+        inputElement.value = inputElement.value.replace(res, '');
+      };
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(formElement, inputList);
+    });
+  });
+};
+
+enableValidation();
