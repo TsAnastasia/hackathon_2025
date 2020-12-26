cc86a48f14665fcd985e19b4dabec01ade0d1e0f
TsAnastasia <72159790+TsAnastasia@users.noreply.github.com>
2020-12-26 13:31:07 +0300

    feat: change header

--- scripts/header.js
+++ scripts/header.js
@@ -1,14 +1,38 @@
+//TO DO: data from server
+const person = {
+  name: "Валисий",
+  countAlert: 1,
+  countInitiative: 245,
+}
+
 const page = document.querySelector('.page');
 const header = document.querySelector('.header');
 const buttonMenu = header.querySelector('.header__button_type_menu');
-const menuContainer = header.querySelector('.header__nav');
+const menuContainer = header.querySelector('.header__menu');
+const countAlert = header.querySelector('.menu__count_type_alert');
+const countInitiatives = header.querySelector('.menu__count_type_initiatives')
+const menuVisibleClass = 'header__menu_visible';
 
-const settingHeader = {
-  navHiddenSelector: 'header__nav_hidden',
-}
-
-const toggleHeaderMenu = () => {
-  menuContainer.classList.toggle(settingHeader.navHiddenSelector);
+const openMenu = () => {
+  countAlert.textContent = person.countAlert;
+  countInitiatives.textContent = person.countInitiative;
+  menuContainer.classList.add(menuVisibleClass);
+  page.addEventListener('click', (evt) => {
+    if (!evt.target.closest('.header__menu')) {
+      closeMenu();
+    };
+  });
 };
 
-buttonMenu.addEventListener('click', toggleHeaderMenu);
+const closeMenu = () => {
+  menuContainer.classList.remove(menuVisibleClass);
+};
+
+buttonMenu.addEventListener('click', (evt) => {
+  evt.stopPropagation();
+  if (menuContainer.classList.contains(menuVisibleClass)) {
+    closeMenu();
+  }else{
+    openMenu();
+  };
+});
