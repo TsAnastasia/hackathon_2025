3bb84cad6d3e6eb5b2811e2f9107d1de960701c9
TsAnastasia <72159790+TsAnastasia@users.noreply.github.com>
2020-12-27 23:33:26 +0300

    feat: change initiatives pages

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,3 @@
-const templaleInitiativeCard = page.querySelector('.templeteInitiativeCard').content;
-const commonCardsContainer = page.querySelector('.initiativesCommon__list');
-const customCardsContainer = page.querySelector('.initiativesCustom__list');
 const templateCategoryItemContainer = page.querySelector('.templateCategoryItem').content;
 const categoryListContainer = page.querySelector('.categoryList');
 
@@ -18,22 +15,6 @@ const toggleCategory = (category) => {
   category.classList.toggle('categoryList__name_active');
   //TO DO: update initiatives list
 }
-
-const createInitiativeCard = (initiative) => {
-  const newCard = templaleInitiativeCard.cloneNode(true);
-  newCard.querySelector('.initiativeCard__image').src = initiative.imageLink;
-  newCard.querySelector('.initiativeCard__title').textContent = initiative.title;
-  newCard.querySelector('.initiativeCard__qoute').textContent = initiative.qoute;
-  newCard.querySelector('.initiativeCard__type').classList.add(`initiativeCard__type_value_${initiative.type}`);
-  newCard.querySelector('.initiativeCard__date').textContent = initiative.date;
-  return newCard;
-};
-
-const addInitiativeCards = (cards, container) => {
-  cards.reverse().forEach((item) => { 
-    container.prepend( createInitiativeCard(item) );
-  });
-};
 
 categories.forEach((category) => {
   categoryListContainer.append(createCategoryItem(category));
