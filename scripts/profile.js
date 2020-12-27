81432392e90f071719f1e88b9ae93b6f555834a4
TsAnastasia <72159790+TsAnastasia@users.noreply.github.com>
2020-12-27 23:04:04 +0300

    feat: change simple pages

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,9 +1,5 @@
-const avatarContainer = page.querySelector('.profile__avatar');
-const namePersonContainer = page.querySelector('.profile__name');
-const typePersonContainer = page.querySelector('.profile__person');
-const countInitiativeContainer = page.querySelector('.profile__menuCount_type_initiative');
-
-avatarContainer.style.backgroundImage = `url("${person.avatarLink}")`;
-namePersonContainer.textContent = person.name;
-typePersonContainer.textContent = person.type;
-countInitiativeContainer.textContent = person.countInitiative;
+page.querySelector('.profile__avatar').style.backgroundImage = `url("${person.avatarLink}")`;
+page.querySelector('.profile__name').textContent = person.name;
+page.querySelector('.profile__person').textContent = person.type;
+page.querySelector('.profile__menuCount_type_initiative').textContent = person.countInitiative;
+page.querySelector('.profile__menuCount_type_alert').textContent = person.countAlert;
