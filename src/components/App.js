97f3f1a8f9d73b0c7d304ece6c2b96d7040b3835
Anastasia Tsibernaya <yfcnz68@yandex.ru>
2021-04-23 12:57:30 +0300

    faet: create app on react

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app__header">
+        <button type="button">menu</button>
+        <a>logo</a>
+        <button type="button">search</button>
+      </header>
+
+      <main className="app__content"></main>
+
+      <footer className="app__footer"></footer>
+    </div>
+  );
+}
+
+export default App;
